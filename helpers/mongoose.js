1f684f5a4f980712a57634bfe9f2e86d5f39304d
bruno moura <zbnmoura@gmail.com>
2020-06-21 15:19:11 -0300

    primary save

--- helpers/mongoose.js
+++ helpers/mongoose.js
@@ -11,28 +11,46 @@ const mongoose_connect = async (current_uri = db_uri) => {
 //timestamp: data_criacao, data_atualizacao, ultimo_login(first = data_criação)
 //token: jwt
 //email unique: get ou fazer "unique"
-const user_schema = new mongoose.Schema({
-    _id: { type: mongoose.Schema.ObjectId, auto: true },
-    nome: String,
-    email: String,
-    senha: String,
-    token:String,
-    telefones: ['numero','ddd'], //FIXME ver a possibilidade de modelar o numero e ddd
-});
-
-//create
-//read
-//update
+const user_schema = new mongoose.Schema(
+    {
+        _id: { type: mongoose.Schema.ObjectId, auto: true },
+        nome: { type: String, lowercase: true },
+        email: { type: String, lowercase: true, trim: true },
+        senha: String,
+        token: String,
+        telefones: [], //FIXME ver a possibilidade de modelar o numero e ddd
+        ultimo_login: { type: Date, default: Date.now },
+    },
+    {
+        timestamps: {
+            createdAt: 'data_criacao',
+            updatedAt: 'data_atualizacao',
+        },
+    },
+);
 
 const User = mongoose.model('User', user_schema);
 
+//create
+const save_doc = async (args, db_uri_test) => {
+    await mongoose_connect(db_uri_test);
+    const { id: _id, nome, email, senha, token, telefones } = await User.create(args);
+    return { id: _id, nome, email, senha, token, telefones };
+};
+//read
+//update
 
-
-
-// (async () => {
-//     try {
-//       
-//     } catch (error) {
-//         console.error({ error });
-//     }
-// })();
+(async () => {
+    try {
+        const x = await save_doc({
+            nome: 'BrUnO',
+            email: 'zbnmoura@gmail.com',
+            senha: '123',
+            token: '123',
+            telefones: [{ numero: '12345', ddd: '123' }],
+        });
+        console.log(x);
+    } catch (error) {
+        console.error({ error });
+    }
+})();
