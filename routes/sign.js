6b5a2eaece5c1081d461db352a147a92df1ff2bd
bruno moura <zbnmoura@gmail.com>
2020-06-22 01:29:01 -0300

    signin

--- routes/sign.js
+++ routes/sign.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const body_parser = require('body-parser');
-const { save_document } = require('../helpers/mongoose');
+const { create_document, read_document } = require('../helpers/mongoose');
 const router = express.Router();
 
 router.post('/up', body_parser.json());
 router.post('/up', async (req, res) => {
     const { nome, email, senha, telefones } = req.body;
     try {
-        const { _id: id, name, phones, token, last_login, created_at, updated_at } = await save_document({
+        const { _id: id, name, phones, token, last_login, created_at, updated_at } = await create_document({
             name: nome,
             email,
             phones: telefones,
@@ -30,36 +30,29 @@ router.post('/up', async (req, res) => {
     }
 });
 
+router.post('/in', body_parser.json());
+router.post('/in', async (req, res) => {
+    const { email, senha } = req.body;
+    try {
+        const document = await read_document({ email, password: senha });
+        if (document === null) {
+            return res.status(401).json({ message: 'Usuário e/ou senha inválidos' });
+        }
+        const result = {
+            token: document.token,
+            id: document._id,
+            email: document.email,
+            nome: document.name,
+            telefones: document.phones,
+            data_criacao: document.created_at,
+            data_atualizacao: document.updated_at,
+            ultimo_login: document.last_login,
+        };
+        return res.status(200).json(result);
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({ message: error });
+    }
+});
+
 module.exports = router;
-
-// const request = {
-//     nome: 'string',
-//     email: 'string',
-//     senha: 'senha',
-//     telefones: [
-//         {
-//             numero: '123456789',
-//             ddd: '11',
-//         },
-//     ],
-// };
-
-// const usuario = {
-//     nome,
-//     email,
-//     telefones,
-// };
-// const response_200 = { usuario, id, data_criacao, data_atualizacao, ultimo_login, token };
-// const response_erro_email = 'E-mail já existente';
-// const response_erro_geral = 'erro geral';
-// sign in
-
-/**
- * Este endpoint irá receber um objeto com e-mail e senha.
-Caso o e-mail exista e a senha seja a mesma que a senha persistida, retornar
-igual ao endpoint de sign_up.
-Caso o e-mail não exista, retornar erro com status apropriado mais a mensagem
-"Usuário e/ou senha inválidos"
-Caso o e-mail exista mas a senha não bata, retornar o status apropriado 401
-mais a mensagem "Usuário e/ou senha inválidos"
- */
