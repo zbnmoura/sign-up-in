7c115b715fe691c4c7ea6830b631d18f3e50b1d5
bruno moura <zbnmoura@gmail.com>
2020-06-22 02:20:49 -0300

    todos

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8081;
 const sign = require('./routes/sign');
+const user = require('./routes/users');
 
 app.use(cors());
 app.use('/sign', sign);
+app.use('/users', user);
 
 app.listen(port, () => console.log(`running on port: ${port}`));
 
 //TODO middleware with Joi to force use email and password (signup)
-//TODO get se o email ja existe na base //"E-mail já existente"
-//TODO
+//TODO get se o email ja existe na base //"E-mail já existente" (signup)
+//TODO tratar erros do mongoose na rota
 //TODO
