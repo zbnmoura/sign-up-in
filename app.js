f1541fa6be38ba356c25c770fa5983bc510eaf7e
bruno moura <zbnmoura@gmail.com>
2020-06-22 09:02:12 -0300

    todos

--- app.js
+++ app.js
@@ -15,4 +15,4 @@ app.listen(port, () => console.log(`running on port: ${port}`));
 //TODO middleware with Joi to force use email and password (signup)
 //TODO get se o email ja existe na base //"E-mail já existente" (signup)
 //TODO tratar erros do mongoose na rota
-//TODO
+//TODO retornar json endpoint nao econtrado
